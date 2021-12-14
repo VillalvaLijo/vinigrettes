fe07e0339a003aa1b02ce3bbbbfb94c18235859a
Sam Lijo <samuelvillalvalijo@gmail.com>
2021-12-13 19:11:43 -0600

    Some random number generation

--- random_num_list.py
+++ random_num_list.py
@@ -3,4 +3,18 @@
 
 #Made this at on my break at work to practice random number generation in python
 
+from random import seed
+from random import random
+
+seed(12000)
+
+rand_list = []
+
+for _ in range(1000):
+    rand_list.append(random())
+
+
+for _ in rand_list:
+    print(_)
+
 
